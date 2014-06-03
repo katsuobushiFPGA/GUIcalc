372df5e6bf41c6ab1d6a4be311dbeef46207e427
katsuobushi <katsuobushiFPGA>
2014-06-03 18:45:47 +0900

    コメンど部分けしました

--- GUICalculator/src/ParserTest.java
+++ GUICalculator/src/ParserTest.java
@@ -5,13 +5,5 @@ class ParserTest {
 		//title,width,height
 		CalcView v = new CalcView("電卓",600,700);
 		v.setLocationRelativeTo(null);
-
-		/* =を押したらパーサ起動、StringにあるものをInputStreamに変換し、double型で返す。 String.valueOf(double)でresultLabelに表示 */
-/*
-			final Reader r = new InputStreamReader(System.in);
-			final Parser parser = new Parser(r);
-			final double tree = parser.start();
-*/
-
     }
 }
